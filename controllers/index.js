526f920af5dc186377d5bc512d17a6ee0a4abe0f
jescatel607 <jacquelinee6072@gmail.com>
2025-08-06 18:54:10 -0600

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const awesomeFunction = (req, res) => {
+  res.send("Tooele Tech is Awesome!");
+};
+
+module.exports = {
+  awesomeFunction,
+};
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers");
+
+router.get("/tech", Controller.awesomeFunction);
+
+module.exports = router;
